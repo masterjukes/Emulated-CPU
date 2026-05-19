0ed2a258288a479d77bf2fedc921e393a4a97183
alfie <alfiejukes4@gmail.com>
2026-05-20 00:01:06 +0100

    first commit. already created a basic outline for handling CPU and Memory

---
+++ Fun CPU/Cpu.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace Fun_CPU;
+
+
+public class Cpu
+{
+    readonly IBus connectedBus;
+    int Fetch()
+    {
+        return 0;
+    }
+    
+    void Decode()
+    {
+        
+    }
+    
+    void Execute()
+    {
+        
+    }
+    
+    void WriteBack()
+    {
+        
+    }
+    
+}
---
+++ Fun CPU/Memory.cs
@@ -0,0 +1,22 @@
+﻿namespace Fun_CPU;
+
+public interface IBus
+{
+    byte ReadByte(int address);
+    void WriteByte(int address, byte value);
+}
+
+public class SimulatedBusController : IBus
+{
+    private byte[] _memory;
+
+    public byte ReadByte(int address)
+    {
+        return _memory[address];
+    }
+
+    public void WriteByte(int address, byte value)
+    {
+        _memory[address] = value;
+    }
+}
